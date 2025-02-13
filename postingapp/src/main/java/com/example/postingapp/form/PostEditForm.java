42a992a717fa7b2d51017bc60f972c1202a074d6
nakanishil <nakanishi.jsdf@gmail.com>
2025-02-13 14:44:03 +0900

    firstCommit

---
+++ postingapp/src/main/java/com/example/postingapp/form/PostEditForm.java
@@ -0,0 +1,21 @@
+package com.example.postingapp.form;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+// 前フィールドに値をセットするための引数つきコンストラクタを自動生成できる:AllArgsConstructor
+@AllArgsConstructor
+public class PostEditForm {
+	
+	@NotBlank(message = "タイトルを入力してください。")
+	@Size(max = 40, message = "タイトルは40文字以内で入力してください。")
+	private String title;
+	
+	@NotBlank(message = "本文を入力してください。")
+	@Size(max = 200, message = "本文は200文字以内で入力してください。")
+	private String content;
+}
